a9c4437f0133054d1d1fc3390f31606d056b9e91
Jianan Sun <isunjn@gmail.com>
2021-06-05 21:49:40 +0800

    better ui code and gameover check logic

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,17 @@ enum Piece {
     O,
 }
 
+static CHECK_POS: [(usize, usize, usize); 8] = [
+    (0, 1, 2),
+    (3, 4, 5),
+    (6, 7, 8),
+    (0, 3, 6),
+    (1, 4, 7),
+    (2, 5, 8),
+    (0, 4, 8),
+    (2, 4, 6),
+];
+
 struct Game {
     board: [Piece; 9],
     pos: (u32, u32),
@@ -36,34 +47,34 @@ impl Game {
             Key::Char('q') => process::exit(0),
             Key::Left => {
                 self.pos.1 = (self.pos.1 - 1 + 3) % 3;
-            },
+            }
             Key::Right => {
                 self.pos.1 = (self.pos.1 + 1) % 3;
-            },
+            }
             Key::Up => {
                 self.pos.0 = (self.pos.0 - 1 + 3) % 3;
-            },
+            }
             Key::Down => {
                 self.pos.0 = (self.pos.0 + 1) % 3;
-            },
+            }
             Key::Char('o') => {
                 if self.turn == Piece::O && self.board[pos] == Piece::Empty {
                     self.board[pos] = Piece::O;
                     self.turn = Piece::X;
                 }
-            },
+            }
             Key::Char('x') => {
                 if self.turn == Piece::X && self.board[pos] == Piece::Empty {
                     self.board[pos] = Piece::X;
                     self.turn = Piece::O;
                 }
-            },
+            }
             _ => {}
         }
     }
 
     fn draw_board(&self) {
-        let mut ox: Vec<&str> = self
+        let mut pieces: Vec<&str> = self
             .board
             .iter()
             .map(|x| match *x {
@@ -73,60 +84,68 @@ impl Game {
             })
             .collect();
         let pos = (self.pos.0 * 3 + self.pos.1) as usize;
-        ox[pos] = match ox[pos] {
+        pieces[pos] = match pieces[pos] {
             " " => "\x1b[34m+\x1b[0m",
             "O" => "\x1b[34mO\x1b[0m",
             "X" => "\x1b[34mX\x1b[0m",
             _ => " ",
         };
 
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
-        print!("\n\r  ────────── Tic-Tac-Toe ──────────  \n\r\n\r");
-        print!(
-            "      {} │ {} │ {}      \x1b[32m↑/↓/←/→\x1b[0m to move\n\r     ───┼───┼───     \x1b[32mo/x\x1b[0m to set piece\n\r      {} │ {} │ {}      \x1b[32mq\x1b[0m to quit\n\r     ───┼───┼───\n\r      {} │ {} │ {}      \x1b[34m{}\x1b[0m's turn\n\r",
-            ox[0], ox[1], ox[2], ox[3], ox[4], ox[5], ox[6], ox[7], ox[8],
+        let ui_1 = format!(
+            "      {} │ {} │ {}      \x1b[32m↑/↓/←/→\x1b[0m to move\n\r",
+            pieces[0], pieces[1], pieces[2]
+        );
+        let ui_2 = format!("     ───┼───┼───     \x1b[32mo/x\x1b[0m to set piece\n\r");
+        let ui_3 = format!(
+            "      {} │ {} │ {}      \x1b[32mq\x1b[0m to quit\n\r",
+            pieces[3], pieces[4], pieces[5]
+        );
+        let ui_4 = format!("     ───┼───┼───\n\r");
+        let ui_5 = format!(
+            "      {} │ {} │ {}      \x1b[34m{}\x1b[0m's turn\n\r",
+            pieces[6],
+            pieces[7],
+            pieces[8],
             match self.turn {
                 Piece::O => 'O',
                 Piece::X => 'X',
                 _ => ' ',
             }
         );
+
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        print!("\n\r  ────────── Tic-Tac-Toe ──────────  \n\r\n\r");
+        print!("{}{}{}{}{}", ui_1, ui_2, ui_3, ui_4, ui_5);
     }
 
     fn is_over(&self) -> bool {
         let board = self.board;
-        if (board[0] == board[1] && board[0] == board[2] && board[1] == board[2] && board[2] == Piece::O)
-        || (board[3] == board[4] && board[3] == board[5] && board[4] == board[5] && board[5] == Piece::O)
-        || (board[6] == board[7] && board[6] == board[8] && board[7] == board[8] && board[8] == Piece::O)
-        || (board[0] == board[4] && board[0] == board[8] && board[4] == board[8] && board[8] == Piece::O)
-        || (board[2] == board[4] && board[2] == board[6] && board[4] == board[6] && board[6] == Piece::O)
-        || (board[0] == board[3] && board[0] == board[6] && board[3] == board[6] && board[6] == Piece::O)
-        || (board[1] == board[4] && board[1] == board[7] && board[4] == board[7] && board[7] == Piece::O)
-        || (board[2] == board[5] && board[2] == board[8] && board[5] == board[8] && board[8] == Piece::O)
-        {
-            print!("\n\r\x1b[34m               O wins!\n\r\x1b[0m");
-            return true;
-        }
-        if (board[0] == board[1] && board[0] == board[2] && board[1] == board[2] && board[2] == Piece::X)
-        || (board[3] == board[4] && board[3] == board[5] && board[4] == board[5] && board[5] == Piece::X)
-        || (board[6] == board[7] && board[6] == board[8] && board[7] == board[8] && board[8] == Piece::X)
-        || (board[0] == board[4] && board[0] == board[8] && board[4] == board[8] && board[8] == Piece::X)
-        || (board[2] == board[4] && board[2] == board[6] && board[4] == board[6] && board[6] == Piece::X)
-        || (board[0] == board[3] && board[0] == board[6] && board[3] == board[6] && board[6] == Piece::X)
-        || (board[1] == board[4] && board[1] == board[7] && board[4] == board[7] && board[7] == Piece::X)
-        || (board[2] == board[5] && board[2] == board[8] && board[5] == board[8] && board[8] == Piece::X)
-        {
-            print!("\n\r\x1b[34m               X wins!\n\r\x1b[0m");
-            return true;
-        }
-
-        for piece in board.iter() {
-            if *piece == Piece::Empty {
-                return false;
+        let mut no_one_can_win = true;
+        for (p1, p2, p3) in CHECK_POS.iter() {
+            match (board[*p1], board[*p2], board[*p3]) {
+                (Piece::O, Piece::O, Piece::O) => {
+                    self.draw_board();
+                    print!("\n\r\x1b[34m               O wins! 🎉\n\r\n\r\x1b[0m");
+                    return true;
+                }
+                (Piece::X, Piece::X, Piece::X) => {
+                    self.draw_board();
+                    print!("\n\r\x1b[34m               X wins! 🎉\n\r\n\r\x1b[0m");
+                    return true;
+                }
+                (p_1, p_2, p_3) => {
+                    if p_1 == Piece::Empty && p_2 == Piece::Empty && p_3 == Piece::Empty {
+                        no_one_can_win = false;
+                    }
+                }
             }
         }
-        print!("\n\r\x1b[34m               Draw!\n\r\x1b[0m");
-        return true;
+        if no_one_can_win {
+            self.draw_board();
+            print!("\n\r\x1b[34m                Draw! 🤝\n\r\n\r\x1b[0m");
+            return true;
+        }
+        false
     }
 }
 
